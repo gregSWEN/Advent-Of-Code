43a7bd3ec99767c64adca3537c1363f955af5763
gregSWEN <69919565+gregSWEN@users.noreply.github.com>
2023-12-02 15:44:21 -0500

    finished part 2

--- 2023/Day2/Day2.py
+++ 2023/Day2/Day2.py
@@ -7,22 +7,41 @@ def main():
         sumLine = 0
         for line in lines:
             line = line.strip().split(":")
-            gameNum = line[0].split(" ")[1]
-            bags = line[1].split(";")
-            for bag in bags:
-                isValid = True
-                print(bag)
-                dict = {"blue": 0, "red": 0, "green": 0}
-                dict = count_numbers(dict, bag)
-                print(dict)
-                if not is_valid(dict):
-                    isValid = False
-                    break
-            if isValid:
-                print(f"Game {gameNum}  being added")
-                sumLine += int(gameNum)
+            # gameNum = line[0].split(" ")[1]
+            redMax = findMax(line[1], "red")
+            blueMax = findMax(line[1], "blue")
+            greenMax = findMax(line[1], "green")
+
+            sumLine += (redMax*blueMax*greenMax)
+
+            # bags = line[1].split(";")
+            # for bag in bags:
+            #     isValid = True
+            #     print(bag)
+            #     dict = {"blue": 0, "red": 0, "green": 0}
+            #     dict = count_numbers(dict, bag)
+            #     print(dict)
+            #     if not is_valid(dict):
+            #         isValid = False
+            #         break
+
+            # if isValid:
+            #     print(f"Game {gameNum}  being added")
+            #     sumLine += int(gameNum)
 
     return sumLine
+
+
+def findMax(line, color):
+    line = line.strip().split(" ")
+    maxRed, i = 0, 0
+    while i < len(line):
+        word = re.sub(r'\W+', '', line[i+1])
+        if word == color:
+            maxRed = max(int(line[i]), maxRed)
+        i += 2
+
+    return maxRed
 
 
 def is_valid(dict):
